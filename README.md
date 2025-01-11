21f082ae6f5ffe28e0ad2952cbe5cbf0a1329c88
Maxime Courchesne <maxime.courchesne@mail.mcgill.ca>
2025-01-10 23:35:52 -0500

    create a Dockerfile to run the video formatter service

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # youtube-clone
+
+### POST request format to create a processed video
+{
+  "inputFilePath":"./test_vid.webm",
+  "outputFilePath":"./processed_test_vid.webm"
+}
+
+## Docker commands
+### build image
+docker build -t video-processor .
+
+### run container from image
+docker run -it -p 3000:3000 localhost/video-processor
+
+## Create new video of 360p resolution
+### copy a file to a running container
+docker cp ./test_vid.webm ca397a327210:/app/
+### send POST request of json format:
+{
+  "inputFilePath":"./test_vid.webm",
+  "outputFilePath":"./processed_test_vid.webm"
+}
+### copy a file from a container
+docker cp ca397a327210:/app/processed_test_vid.webm ./
--- video-processing-service/dist/index.js
+++ video-processing-service/dist/index.js
@@ -1,34 +1,33 @@
-import express from "express";
-import ffmpeg from "fluent-ffmpeg";
-
-const app = express()
-app.use(express.json());
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const fluent_ffmpeg_1 = __importDefault(require("fluent-ffmpeg"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
 // app.get("/", (req, res) => {
 //     res.send("Hello World");
 // });
 app.post("/process-video", (req, res) => {
     const inputFilePath = req.body.inputFilePath;
     const outputFilePath = req.body.outputFilePath;
-
     if (!inputFilePath || !outputFilePath) {
         res.status(400).send("Bad Request: Missing file path.");
     }
-    ffmpeg(inputFilePath).outputOptions("-vf", "scale=-1:360") // convert video to 360p
-    .on("end", function() {
+    (0, fluent_ffmpeg_1.default)(inputFilePath).outputOptions("-vf", "scale=-1:360") // convert video to 360p
+        .on("end", function () {
         console.log("Processing finished successfully");
         res.status(200).send("Video processing started.");
     })
-    .on("error", (err) => {
+        .on("error", (err) => {
         console.log(`An error occured: ${err.message}`);
         res.status(500).send(`Internal Server Error: ${err.message}`);
     })
-    .save(outputFilePath);
+        .save(outputFilePath);
 });
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`video processing service listening at http://localhost:${port}`);
-})
-
-
+});
